8b6696eb1b16c5a0f3a11b7b4f8e0af6d1c42db6
sangala632 <sangala2003@gmail.com>
2026-05-11 00:27:28 +0530

    some configuration changes

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
-    var const = {
+    const stat = {
         app: 'OK',
         mongo: mongoConnected
     };
